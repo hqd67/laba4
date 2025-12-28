0fb9760e886d37835dcaf17deb9302f6a470e017
артем <artemlemur704@gmail.com>
2025-12-28 08:06:03 +0300

    Добавление модели данных для рисования

---
+++ laba4/Models/DrawPoint.cs
@@ -0,0 +1,22 @@
+﻿namespace OnlineWhiteboard.Models
+{
+    public enum DrawTool
+    {
+        Pen,
+        Line,
+        Rectangle,
+        Ellipse,
+        Clear
+    }
+
+    public class DrawPoint
+    {
+        public int X1 { get; set; }
+        public int Y1 { get; set; }
+        public int X2 { get; set; }
+        public int Y2 { get; set; }
+        public string Color { get; set; }
+        public int Thickness { get; set; }
+        public DrawTool Tool { get; set; }
+    }
+}
